6d30dbcd044f024ceff2abf9775a1512a86369c8
NithinTellakula <nithintellakula2003@gmail.com>
2025-07-18 12:13:43 +0530

    "java file update"

---
+++ src/main/java/com/example/gitactions/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.gitactions;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Application is running!";
+    }
+}
+
