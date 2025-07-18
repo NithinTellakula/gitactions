b66345a1f4120939c2b916ce89ced273d4a51ca4
NithinTellakula <nithintellakula2003@gmail.com>
2025-07-18 12:50:05 +0530

    "html update"

--- src/main/java/com/example/gitactions/HomeController.java
+++ src/main/java/com/example/gitactions/HomeController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping("/")
-    public String home() {
-        return "Application is running!";
+    @GetMapping("/status")
+        public String status() {
+            return "Application is running!";
+        }
     }
-}
+
+
 
